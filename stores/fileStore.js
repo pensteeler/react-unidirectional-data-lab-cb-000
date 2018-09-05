9c14745a16c60ef11e836b3ce8d4f4f85d5b122f
John Vascsinec <pensteeler@gmail.com>
2018-09-05 16:10:50 +0000

    automatically backed up by learn

--- stores/fileStore.js
+++ stores/fileStore.js
@@ -2,7 +2,28 @@
 
 import Store from './Store';
 
-class FileStore {
+class FileStore extends Store {
+  
+  updateFile(index, file) {
+    const files = this.getState().slice();
+    files.splice(index, 1, file);
+    this.setState(files);
+  }
+
+  addFile() {
+    const files = this.getState().concat('');
+    this.setState(files);
+  }
+
+  removeFile(index) {
+    if (this.state.length === 1) {
+      return this.setState(['']);
+    }
+
+    const files = this.getState().slice()
+    files.splice(index, 1);
+    this.setState(files);
+  }
 }
 
 const fileStore = new FileStore([]);
